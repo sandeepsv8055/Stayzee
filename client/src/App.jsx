0ab3c268d1aacc88b427006527ef2c000add33bd
sandeepsv8055 <143858545+sandeepsv8055@users.noreply.github.com>
2025-04-08 15:58:23 +0530

    yuf

---
+++ client/src/App.jsx
@@ -0,0 +1,32 @@
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <div>
+      <header className='p-4 flex justify-between'>
+        <a href="" className="flex items-center gap-1">
+        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-8">
+           <path strokeLinecap="round" strokeLinejoin="round" d="m2.25 12 8.954-8.955c.44-.439 1.152-.439 1.591 0L21.75 12M4.5 9.75v10.125c0 .621.504 1.125 1.125 1.125H9.75v-4.875c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21h4.125c.621 0 1.125-.504 1.125-1.125V9.75M8.25 21h8.25" />
+        </svg>
+
+        <span className='font-bold text-xl'>Stayzee</span>
+        </a>
+        <div className='flex'> 
+          <div>Anywhere</div>
+          <div>Any week</div>
+          <div>Add guests</div>
+          <button>
+          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
+            <path strokeLinecap="round" strokeLinejoin="round" d="m21 21-5.197-5.197m0 0A7.5 7.5 0 1 0 5.196 5.196a7.5 7.5 0 0 0 10.607 10.607Z" />
+          </svg>
+
+          </button>
+        </div>
+      </header>
+    </div> 
+  )
+}
+
+export default App
