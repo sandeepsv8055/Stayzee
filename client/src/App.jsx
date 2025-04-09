efa09504ede3214fdfdcf97f3ae6da6850eaa47e
sandeepsv8055 <143858545+sandeepsv8055@users.noreply.github.com>
2025-04-09 12:12:23 +0530

    .

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -13,16 +13,29 @@ function App() {
 
         <span className='font-bold text-xl'>Stayzee</span>
         </a>
-        <div className='flex'> 
+        <div className='flex gap-2 border border-grey-300 rounded-full py-2 px-4 shadow-md shadow-gray-300'> 
           <div>Anywhere</div>
+          <div className=" border-l border-gray-300"></div>
           <div>Any week</div>
+          <div className=" border-l border-gray-300"></div>
           <div>Add guests</div>
-          <button>
-          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
+          <button className='bg-primary text-white p-1 rounded-full'>
+          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-4">
             <path strokeLinecap="round" strokeLinejoin="round" d="m21 21-5.197-5.197m0 0A7.5 7.5 0 1 0 5.196 5.196a7.5 7.5 0 0 0 10.607 10.607Z" />
           </svg>
 
           </button>
+        </div>
+        <div className='flex gap-2 border border-grey-300 rounded-full py-2 px-4 '>
+          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
+            <path strokeLinecap="round" strokeLinejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
+          </svg>
+
+          <div className="bg-gray-500 text-white">
+          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
+            <path strokeLinecap="round" strokeLinejoin="round" d="M15.75 6a3.75 3.75 0 1 1-7.5 0 3.75 3.75 0 0 1 7.5 0ZM4.501 20.118a7.5 7.5 0 0 1 14.998 0A17.933 17.933 0 0 1 12 21.75c-2.676 0-5.216-.584-7.499-1.632Z" />
+          </svg>
+          </div>
         </div>
       </header>
     </div> 
--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -5,7 +5,11 @@ export default {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: '#F53850'
+      }
+    },
   },
   plugins: [],
 }
