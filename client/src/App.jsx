aada31af11d039ed4af9ad64ba1658430375f2fe
sandeepsv8055 <143858545+sandeepsv8055@users.noreply.github.com>
2025-04-10 12:26:06 +0530

    nxt begin add react router

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -26,12 +26,12 @@ function App() {
 
           </button>
         </div>
-        <div className='flex gap-2 border border-grey-300 rounded-full py-2 px-4 '>
+        <div className='flex items-center gap-2 border border-grey-300 rounded-full py-2 px-4 '>
           <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
             <path strokeLinecap="round" strokeLinejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
           </svg>
           {/* user sign */}
-          <div className="bg-gray-500 text-white rounded-full border border-gray-500">
+          <div className="bg-gray-500 text-white rounded-full border border-gray-500 overflow-hidden">
             <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="size-6 relative -top-0">
               <path fillRule="evenodd" d="M18.685 19.097A9.723 9.723 0 0 0 21.75 12c0-5.385-4.365-9.75-9.75-9.75S2.25 6.615 2.25 12a9.723 9.723 0 0 0 3.065 7.097A9.716 9.716 0 0 0 12 21.75a9.716 9.716 0 0 0 6.685-2.653Zm-12.54-1.285A7.486 7.486 0 0 1 12 15a7.486 7.486 0 0 1 5.855 2.812A8.224 8.224 0 0 1 12 20.25a8.224 8.224 0 0 1-5.855-2.438ZM15.75 9a3.75 3.75 0 1 1-7.5 0 3.75 3.75 0 0 1 7.5 0Z" clipRule="evenodd" />
             </svg>
